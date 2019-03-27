a2e1351ad6a25e94147a986e6431f7c0fd75e891
Risky Muaji <muaji.risky@gmail.com>
2019-03-27 16:43:05 +0700

    Delete koneksi-DESKTOP-07RDASM.php

--- koneksi-DESKTOP-07RDASM.php
+++
@@ -1,9 +0,0 @@
-<?php
-	
-	define('db_host','localhost');
-	define('db_user','root');
-	define('db_pass',"");
-	define('db_name','qmusik');
-	
-	mysql_connect(db_host,db_user,db_pass) or die (mysql_error());
-	mysql_select_db(db_name) or die (mysql_error());
